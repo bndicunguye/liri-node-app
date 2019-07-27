481fd7256aadf40ce68a5db731d4520864e8e19c
Mayenga <bndicunguye@gmail.com>
2019-07-27 17:03:12 -0400

    adding initial code for spotfy

--- liri.js
+++ liri.js
@@ -7,26 +7,21 @@ var axios = require("axios");
 var moment = require("moment");
 
 var userSearch = "";
-for(var i = 3; i<process.argv.length; i++){
-    if(i !== 3){
+for (var i = 3; i < process.argv.length; i++) {
+    if (i !== 3) {
         userSearch += " "
     }
     userSearch += process.argv[i]
 }
 
-var keys = require("./keys.js");
-
-var Spotify = require('node-spotify-api');
-
-var spotify = new Spotify(keys.spotify);
-
 var search = process.argv[2]
 
-console.log(search)
+require("dotenv").config();
+
 
 switch (search) {
     case "movie-this":
-       movie()
+        movie()
         break;
     case "concert-this":
         console.log("concert")
@@ -38,26 +33,62 @@ switch (search) {
         console.log("do what?")
         break;
 
+}
+function movie() {
+    console.log("movie", userSearch)
+    axios.get("http://www.omdbapi.com/?t=" + userSearch + "=&plot=short&apikey=trilogy")
+        .then(function (response) {
+            console.log('* Title of the movie : ' + response.data.Title)
+            console.log('* Year the movie came out : ' + response.data.Released)
+            console.log('* IMDB Rating of the movie. : ' + response.data.imdbRating)
+            console.log('* Rotten Tomatoes Rating of the movie. : ' + response.data.Ratings)
+            console.log('*  Country where the movie was produced. : ' + response.data.Country)
+            console.log('* Language of the movie.: ' + response.data.Language)
+            console.log('* Plot of the movie. : ' + response.data.Plot)
+            console.log('* Actors in the movie. : ' + response.data.Actors)
+
+
+
+        });
 
 }
-function movie(){
-    console.log("movie",userSearch)
-    axios.get("http://www.omdbapi.com/?t="+userSearch+"=&plot=short&apikey=trilogy")
-    .then(function(response){
-        console.log('* Title of the movie : '+ response.data.Title)
-        console.log('* Year the movie came out : '+ response.data.Released)
-        console.log('* IMDB Rating of the movie. : '+ response.data.imdbRating)
-        console.log('* Rotten Tomatoes Rating of the movie. : '+ response.data.Ratings)
-        console.log('*  Country where the movie was produced. : '+ response.data.Country)
-        console.log('* Language of the movie.: '+ response.data.Language)
-        console.log('* Plot of the movie. : '+ response.data.Plot)
-        console.log('* Actors in the movie. : '+ response.data.Actors)
-      
-        
 
-    });
+var Spotify = require('node-spotify-api');
 
-}
+var keys = require("./keys.js");
+
+var spotify = new Spotify(keys.spotify);
+
+
+    spotify
+
+        .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
+
+        .then(function (data) {
+            console.log(data);
+        })
+
+        .catch(function (err) {
+            console.error('Error occurred: ' + err);
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //  `concert-this`
 
